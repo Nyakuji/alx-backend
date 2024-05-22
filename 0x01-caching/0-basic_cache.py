f22ccf6c7ee7e36d201ff25052f945bf0ad7518e
philip Opiyo <opiyophilipo@gmail.com>
2024-05-22 11:02:57 +0300

    refactor: Improve BasicCache class implementation
    Refactor the BasicCache class in 0-basic_cache.py to improve code readability and maintainability. The put method now includes a docstring to explain its purpose and arguments. Additionally, the get method has been simplified using the get method of the cache_data dictionary. These changes enhance the caching functionality in the project.

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python3
-""" Basic dictionary """
-Basecache = __import__('base_caching').BaseCaching
+"""Basic dictionary"""
+
+BaseCache = __import__('base_caching').BaseCaching
 
 
-class BasicCache(Basecache):
-    """ BasicCache class """
+class BasicCache(BaseCache):
+    """ Defines a basic cache system """
 
     def put(self, key, item):
-        """ Add an item in the cache """
+        """
+        Assign item value for the key in
+        self.cache_data
+        if neither key nor item is None
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """return the value in self.cache_data linked to key"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
-
-    def print_cache(self):
-        """ Print the cache data """
-        for key, value in self.cache_data.items():
-            print(f"{key}: {value}")
+        """
+        Return the value linked to key in self.cache_data.
+        Return None if key is None or doesn't exist
+        """
+        return self.cache_data.get(key, None)
