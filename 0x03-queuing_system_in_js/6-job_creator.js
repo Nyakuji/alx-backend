c7bb13d380835973f462df0ed23f36ac9b9e1024
philip Opiyo <opiyophilipo@gmail.com>
2024-06-26 13:48:09 +0300

    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,25 @@
+import kue from 'kue';
+
+// Create the queue
+const queue = kue.createQueue();
+
+// Job data
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'This is a push notification test',
+};
+
+// Create a job
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+// Job event listeners
+job.on('complete', () => {
+  console.log('Notification job completed');
+}).on('failed', () => {
+  console.log('Notification job failed');
+});
